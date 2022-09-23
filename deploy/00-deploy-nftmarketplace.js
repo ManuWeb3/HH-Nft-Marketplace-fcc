876600ef9851ae6bc155b051e732dd7630997219
ManuWeb3 <manu.web3.0@gmail.com>
2022-09-22 21:39:34 -0600

    Initial Commit

--- deploy/00-deploy-nftmarketplace.js
+++ deploy/00-deploy-nftmarketplace.js
@@ -1,12 +1,14 @@
-const {network, ethers} = require("hardhat")
-const {developmentChains, networkConfig} = require("../helper-hardhat-config.js")
+const {network} = require("hardhat")
+const {developmentChains} = require("../helper-hardhat-config.js")
 const {verify} = require("../utils/verify")
+// 'networkConfig' and chainId of helper.js not really needed this time...
+//  as there are no args to be pulled off it into our NFT-MP deploy script.
 
 module.exports = async function ({getNamedAccounts, deployments}) {     // get auto-pulled from hre, hence, all-time available
     const {deploy, log} = deployments
     const {deployer} = await getNamedAccounts()
 
-    const chainId = network.config.chainId
+    // const chainId = network.config.chainId
 
     // deploy now
     console.log("------Deploying NftMarketplace.sol--------")
